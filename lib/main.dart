f9209fdee51de387806d6d2d2a46f733ca72f2be
BasmaMounir <basmamounir57@gimal.com>
2024-04-14 01:17:39 +0200

    create splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_book/features/splash/presntation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
+      home: SplashView(),
     );
   }
 }
